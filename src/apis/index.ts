4a65ca8f90f3dc38f658b52eee187f0b17554942
Jon Church <me@jonchurch.com>
2023-05-11 22:41:59 -0400

    generate with new config
    Date=string, no pure Date types
    models and apis dirs

---
+++ src/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AgentsApi';
+export * from './ContractsApi';
+export * from './DefaultApi';
+export * from './FactionsApi';
+export * from './FleetApi';
+export * from './SystemsApi';
