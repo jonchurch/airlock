3c1111049db5dd1509316b7a1071fe76e194d522
Jon Church <me@jonchurch.com>
2025-02-20 01:34:10 -0500

    regenerate client

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -2,7 +2,8 @@
 /* eslint-disable */
 export * from './AgentsApi';
 export * from './ContractsApi';
-export * from './DefaultApi';
+export * from './DataApi';
 export * from './FactionsApi';
 export * from './FleetApi';
+export * from './GlobalApi';
 export * from './SystemsApi';
